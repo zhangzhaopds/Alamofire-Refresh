53992e87196aaa20fd1f62721333abf6a926ca7e
zhangzhaopds <zhangzhaopds@foxmail.com>
2016-01-11 10:58:49 +0800

    Initial Commit

---
+++ Alamofire网络请求/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alamofire网络请求
+//
+//  Created by 张昭 on 16/1/11.
+//  Copyright © 2016年 张昭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
